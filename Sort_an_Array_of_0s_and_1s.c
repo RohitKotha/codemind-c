64ec63c153dfbe1d403903d4d9770435662b1e30
RohitKotha <119722685+RohitKotha@users.noreply.github.com>
2023-05-19 22:19:57 +0530

    #include <stdio.h>
    void segregateArray(int n, int arr[]) {
        int left = 0;
        int right = n - 1;
    
        while (left < right) {
            while (arr[left] == 0 && left < right)
                left++;
            while (arr[right] == 1 && left < right)
                right--;
    
            if (left < right) {
                arr[left] = 0;
                arr[right] = 1;
                left++;
                right--;
            }
        }
    }
    
    int main() {
        int n;
      
        scanf("%d", &n);
    
        int arr[n];
       
        for (int i = 0; i < n; i++) {
            scanf("%d", &arr[i]);
        }
    
        segregateArray(n, arr);
    
        
        for (int i = 0; i < n; i++) {
            printf("%d ", arr[i]);
        }
    
    
        return 0;
    }

--- Sort_an_Array_of_0s_and_1s.c
+++ Sort_an_Array_of_0s_and_1s.c
@@ -1,22 +1,42 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void segregateArray(int n, int arr[]) {
+    int left = 0;
+    int right = n - 1;
+
+    while (left < right) {
+        while (arr[left] == 0 && left < right)
+            left++;
+        while (arr[right] == 1 && left < right)
+            right--;
+
+        if (left < right) {
+            arr[left] = 0;
+            arr[right] = 1;
+            left++;
+            right--;
+        }
+    }
+}
+
 int main() {
-	int n;
-	scanf("%d",&n);
-	int a[n],b[n];
-	for (int i=0;i<n;i++) {
-		scanf("%d",&a[i]);
-	}
-	for (int i=0;i<(n-1);i++) {
-		for (int j=0;j<(n-1);j++) {
-			if (a[j]>a[j+1]) {
-				a[j]=a[j]+a[j+1];
-				a[j+1]=a[j]-a[j+1];
-				a[j]=a[j]-a[j+1];
-			}
-		}
-	}
-	for (int i=0;i<n;i++) {
-		printf("%d ",a[i]);
-	}
+    int n;
+  
+    scanf("%d", &n);
+
+    int arr[n];
+   
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    segregateArray(n, arr);
+
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+
     return 0;
 }
