88226d4202fe9e72e570c3d6ad3de56c401fe86a
RohitKotha <119722685+RohitKotha@users.noreply.github.com>
2023-09-01 22:27:25 +0530

    #include <stdio.h>
    int main() {
        int T;
        scanf("%d", &T);
    
        while (T--) {
            long long N, A, B, K;
            scanf("%lld %lld %lld %lld", &N, &A, &B, &K);
    
            // Calculate problems solved by Venkat (divisible by B but not divisible by A)
            long long problems_by_Venkat = N / B - (N / (A * B));
    
            // Calculate problems solved by Franklin (divisible by A but not divisible by B)
            long long problems_by_Franklin = N / A - (N / (A * B));
    
            if (problems_by_Venkat + problems_by_Franklin >= K) {
                printf("Win
    ");
            } else {
                printf("Lose
    ");
            }
        }
    
        return 0;
    }

--- Appy_and_Contest.c
+++ Appy_and_Contest.c
@@ -1,18 +1,27 @@
-#include<stdio.h>
-int main()
-{
-    int tc,n,a,b,k,c,i;
-    scanf("%d",&tc);
-    for(i=1;i<=tc;i++)
-    {
-        scanf("%d%d%d%d",&n,&a,&b,&k);
-        c=(n/a)+(n/b);
-        if(c>=k)
-        printf("Win
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        long long N, A, B, K;
+        scanf("%lld %lld %lld %lld", &N, &A, &B, &K);
+
+        // Calculate problems solved by Venkat (divisible by B but not divisible by A)
+        long long problems_by_Venkat = N / B - (N / (A * B));
+
+        // Calculate problems solved by Franklin (divisible by A but not divisible by B)
+        long long problems_by_Franklin = N / A - (N / (A * B));
+
+        if (problems_by_Venkat + problems_by_Franklin >= K) {
+            printf("Win
 ");
-        else
-        printf("Lose
+        } else {
+            printf("Lose
 ");
+        }
     }
-    
+
+    return 0;
 }
